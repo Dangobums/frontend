e3a74d1715b81c8116d8ee43d57f0b9f74eb022e
dangobums <dangobums@pop-os.localdomain>
2022-06-12 18:40:25 +0700

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port:3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7070',
+        ws: true,
+        changeOrigin:true,
+      }
+    }
+  }
 })
